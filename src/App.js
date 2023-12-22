81e8a405c8487dbbcab32453d46ca0043ab7d144
Avinash Tiwari <aviprf10@gmail.com>
2023-12-22 06:24:51 +0530

    flag code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+// App.js
+
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const API_ENDPOINT = 'https://restcountries.com/v3.1/all';
+
+const CountryCard = ({ country }) => {
+  const { name, flags } = country;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="country-card">
+      <img src={flags.svg} alt={`${name.common} Flag`} />
+      <p>{name.common}</p>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(API_ENDPOINT);
+        const data = await response.json();
+        setCountries(data);
+      } catch (error) {
+        console.error('Error fetching data:', error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="app">
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="country-list">
+          {countries.map((country, index) => (
+            <CountryCard key={index} country={country} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
